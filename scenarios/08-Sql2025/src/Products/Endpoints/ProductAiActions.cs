f62a0648ca15481edb831abeb403487e53c5192e
Bruno Capuano <bcapuano@gmail.com>
2025-06-20 17:34:11 -0400

    Update AISearch method for product query improvements
    DETAILS
    
    Modified console output in `AISearch` to reflect "similar products" and added a filter to exclude products with IDs less than or equal to 0, ensuring only valid products are returned in the search results.

--- scenarios/08-Sql2025/src/Products/Endpoints/ProductAiActions.cs
+++ scenarios/08-Sql2025/src/Products/Endpoints/ProductAiActions.cs
@@ -8,16 +8,14 @@ public static class ProductAiActions
 {
     public static async Task<IResult> AISearch(string search, Context db, EmbeddingClient embeddingClient)
     {
-        //var result = await mc.Search(search, db);
-
-
-        Console.WriteLine("Querying for similar posts...");
+        Console.WriteLine("Querying for similar products...");
         float[] vector = embeddingClient.GenerateEmbedding(search).Value.ToFloats().ToArray();
         var relatedProducts = await db.Product
-            .OrderBy(p => EF.Functions.VectorDistance("cosine", p.Embedding, vector))
-            .Select(p => new { p.Name, Distance = EF.Functions.VectorDistance("cosine", p.Embedding, vector) })
-            .Take(3)
-            .ToListAsync();
+                .Where(p => p.Id > 0)
+                .OrderBy(p => EF.Functions.VectorDistance("cosine", p.Embedding, vector))
+                .Select(p => new { p.Name, Distance = EF.Functions.VectorDistance("cosine", p.Embedding, vector) })
+                .Take(3)
+                .ToListAsync();
 
         return Results.Ok(relatedProducts);
     }
